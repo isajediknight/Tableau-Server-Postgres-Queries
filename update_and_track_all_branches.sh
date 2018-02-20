a5a7d1ed2112e460d8904e120477e2e77ff92aad
LB023593 <luke.brady@cerner.com>
2018-02-20 11:46:45 -0600

    Writting script to update and track all branches from their remote origin

---
+++ update_and_track_all_branches.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ehco "< If you have any uncommited changes this script will fail >"
+git checkout master
+sh delete_all_branches_locally.sh
+git fetch origin
+sh pull_and_track_all_branches.sh
+echo "< Be sure to change back to your original Feature Branch >"

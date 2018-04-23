cc72b3e2282711b9d31f32ea878f7d705227f27a
Luke Brady <luke@isajediknight.com>
2018-04-23 12:35:22 -0500

    Adding Join for _background_tasks

--- Custom_SQL/Tableau_Subscription_Runtimes.sql
+++ Custom_SQL/Tableau_Subscription_Runtimes.sql
@@ -1,51 +1,54 @@
-SELECT
-D1.created_at AS "Started",
-CAST(EXTRACT(SECOND FROM (D1.completed_at - D1.created_at)) AS INT) AS "Runtime_Seconds",
-D1.notes AS "Subscription_Results",
+SELECT DISTINCT
+T28.created_at AS "Started",
+CAST(EXTRACT(SECOND FROM (T28.completed_at - T28.created_at)) AS INT) AS "Runtime_Seconds",
+T28.notes AS "Subscription_Results",
 D4.name AS "Subscriber",
 D3.name AS "Subscribed_By",
-D2.subject AS "Subscribed_Content",
+T267.subject AS "Subscribed_Content",
 CASE
-WHEN (D7.customized_view_id IS NULL AND false OR NOT D7.customized_view_id IS NULL AND NOT false) AND (D5.subscription_id IS NULL AND false OR NOT D5.subscription_id IS NULL AND NOT false) THEN 'Customized View - Single View'::text
-WHEN (D7.customized_view_id IS NULL AND false OR NOT D7.customized_view_id IS NULL AND NOT false) AND (D6.subscription_id IS NULL AND false OR NOT D6.subscription_id IS NULL AND NOT false) THEN 'Customized View - Workbook'::text
-WHEN D5.subscription_id IS NULL AND false OR NOT D5.subscription_id IS NULL AND NOT false THEN 'Single View'::text
-WHEN D6.subscription_id IS NULL AND false OR NOT D6.subscription_id IS NULL AND NOT false THEN 'Workbook'::text
+WHEN (T268.customized_view_id IS NULL AND false OR NOT T268.customized_view_id IS NULL AND NOT false) AND (T269.subscription_id IS NULL AND false OR NOT T269.subscription_id IS NULL AND NOT false) THEN 'Customized View - Single View'::text
+WHEN (T268.customized_view_id IS NULL AND false OR NOT T268.customized_view_id IS NULL AND NOT false) AND (T270.subscription_id IS NULL AND false OR NOT T270.subscription_id IS NULL AND NOT false) THEN 'Customized View - Workbook'::text
+WHEN T269.subscription_id IS NULL AND false OR NOT T269.subscription_id IS NULL AND NOT false THEN 'Single View'::text
+WHEN T270.subscription_id IS NULL AND false OR NOT T270.subscription_id IS NULL AND NOT false THEN 'Workbook'::text
 ELSE 'Unknown'::text
 END AS "Content_Type",
 CASE
-WHEN D13.name IS NULL THEN D10.name
-ELSE D13.name
+WHEN T301.name IS NULL THEN D10.name
+ELSE T301.name
 END AS "Workbook",
 CASE
-WHEN D5.repository_url IS NULL THEN D6.repository_url
-ELSE regexp_replace(D5.repository_url, '/sheets'::text, ''::text)
+WHEN T269.repository_url IS NULL THEN T270.repository_url
+ELSE regexp_replace(T269.repository_url, '/sheets'::text, ''::text)
 END AS "URL",
 CASE 
 WHEN D11.name IS NULL THEN D12.name
 ELSE D11.name
 END AS "Workbook Owner",
-D1.site_id AS "Background_Jobs_Site_ID",
-D2.site_id AS "Subscription_Site_ID",
-D2.is_refresh_extract_triggered AS "Extract_Refreshed",
-D1.processed_on_worker AS "Worker",
-D2.ID AS "Subscription_ID",
-D1.id AS "Background_Jobs_ID",
-D7.customized_view_id AS "Customized_View_ID"
---regexp_replace(D5.repository_url, '/sheets'::text, ''::text) AS "View_URL",
---D6.repository_url AS "Workbook_URL",
+T28.site_id AS "Background_Jobs_Site_ID",
+T267.site_id AS "Subscription_Site_ID",
+T267.is_refresh_extract_triggered AS "Extract_Refreshed",
+T28.processed_on_worker AS "Worker",
+T267.ID AS "Subscription_ID",
+T28.id AS "Background_Jobs_ID",
+T268.customized_view_id AS "Customized_View_ID"
+--Add T28. Columns Here
+--Add T1. Columns Here
+--regexp_replace(T269.repository_url, '/sheets'::text, ''::text) AS "View_URL",
+--T270.repository_url AS "Workbook_URL",
 FROM
-background_jobs D1
-INNER JOIN subscriptions D2 ON D2.id = D1.correlation_id
-INNER JOIN system_users D3 ON D3.ID = D2.creator_id
-INNER JOIN system_users D4 ON D4.ID = D2.user_id
-LEFT JOIN subscriptions_views D5 ON D2.id = D5.subscription_id
-LEFT JOIN subscriptions_workbooks D6 ON D2.id = D6.subscription_id
-LEFT JOIN subscriptions_customized_views D7 ON D2.id = D7.subscription_id
-LEFT JOIN views D8 ON D5.repository_url = D8.repository_url--D5.id = D8.id doesnt work
-LEFT JOIN workbooks D10 ON D8.workbook_id = D10.id
+background_jobs T28
+INNER JOIN subscriptions T267 ON T267.id = T28.correlation_id
+INNER JOIN system_users D3 ON D3.ID = T267.creator_id
+INNER JOIN system_users D4 ON D4.ID = T267.user_id
+LEFT JOIN subscriptions_views T269 ON T267.id = T269.subscription_id
+LEFT JOIN subscriptions_workbooks T270 ON T267.id = T270.subscription_id
+LEFT JOIN subscriptions_customized_views T268 ON T267.id = T268.subscription_id
+LEFT JOIN views T297 ON T269.repository_url = T297.repository_url--T269.id = T297.id doesnt work
+LEFT JOIN workbooks D10 ON T297.workbook_id = D10.id
 LEFT JOIN system_users D12 ON D12.id = D10.owner_id
-LEFT JOIN workbooks D13 ON D13.repository_url = D6.repository_url--D13.id = D6.id
-LEFT JOIN system_users D11 ON D11.id = D13.owner_id
+LEFT JOIN workbooks T301 ON T301.repository_url = T270.repository_url--T301.id = T270.id
+LEFT JOIN system_users D11 ON D11.id = T301.owner_id
+LEFT JOIN _background_tasks T1 ON T1.backgrounder_id = T28.backgrounder_id
 WHERE
-D1.job_name = 'Subscription Notifications'
-ORDER BY D1.created_at ASC
+T28.job_name = 'Subscription Notifications'
+ORDER BY T28.created_at ASC

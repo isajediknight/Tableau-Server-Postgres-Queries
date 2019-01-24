1ca50e821b86a6fa31a3a3b0ed457d3975e62fbe
isajediknight <luke@isajediknight.com>
2019-01-24 12:48:47 -0600

    Correcting Issue #1 (fix via Redrichmond)

--- Views/_background_tasks.sql
+++ Views/_background_tasks.sql
@@ -16,27 +16,27 @@ SELECT background_jobs.backgrounder_id,
   background_jobs.language,
   background_jobs.site_id,
   background_jobs.locale
- FROM background_jobs
+ FROM OPENQUERY(TABLEAU,'select * from public.background_jobs')background_jobs
 UNION
 SELECT async_jobs.worker AS backgrounder_id,
   async_jobs.id,
   async_jobs.created_at,
   async_jobs.completed_at,
       CASE
-          WHEN async_jobs.success = true THEN 0
+          WHEN async_jobs.success = 1 THEN 0
           ELSE 1
       END AS finish_code,
   async_jobs.job_type,
   async_jobs.progress,
-  NULL::text AS args,
+  -NULL AS args,
   async_jobs.notes,
   async_jobs.created_at AS started_at,
-  async_jobs.job_type AS job_name,
+  async_jobs.job_type AS job_name ,
   0 AS priority,
-  NULL::character varying(255) AS title,
+  NULL AS title,
   async_jobs.worker AS processed_on_worker,
-  NULL::character varying(255) AS subtitle,
-  NULL::character varying(255) AS language,
+  NULL AS subtitle,
+  NULL AS language,
   async_jobs.site_id,
-  NULL::character varying(255) AS locale
- FROM async_jobs;
+  NULL AS locale
+FROM OPENQUERY(TABLEAU,'select * from public.async_jobs')async_jobs
